9878d720b916c2f711d9b392381d95e977c2df79
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2016-10-07 11:06:02 +0200

    added basic tree collection class

---
+++ GenericCore/Trees/Common/ITreeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Trees.Common
+{
+    public interface ITreeNode<T>
+    {
+        void Add(T node);
+        void Traverse(Node<T> node, Action<T> predicate);
+    }
+}
---
+++ GenericCore/Trees/Common/Tree.cs
@@ -0,0 +1,27 @@
+﻿using GenericCore.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Trees.Common
+{
+    public class Tree<T>
+    {
+        private Node<T> _root;
+
+        public Node<T> Root
+        {
+            get
+            {
+                return _root;
+            }
+        }
+
+        public Tree(T rootValue)
+        {
+            rootValue.AssertNotNull("rootValue");
+            _root = new Node<T>(rootValue);
+        }
+    }
+}
