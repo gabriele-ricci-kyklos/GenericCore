2420683157a153332fa1dbe10c786aaa844cdf9c
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2016-10-27 17:09:12 +0200

    improved existing test cases

--- GenericCore.Test/CollectionExtensionMethodsTests.cs
+++ GenericCore.Test/CollectionExtensionMethodsTests.cs
@@ -18,10 +18,9 @@ namespace GenericCore.Test
             public bool IsActive { get; set; }
         }
 
-        [TestMethod]
-        public void TestToTupleGeneratesNotNullListAndWithSameLengthOneTypes()
+        private IList<Item> GenerateTestList()
         {
-            IList<Item> list =
+            return
                 new List<Item>
                 {
                     new Item
@@ -46,7 +45,12 @@ namespace GenericCore.Test
                         IsActive = true
                     }
                 };
+        }
 
+        [TestMethod]
+        public void TestToTupleGeneratesNotNullListAndWithSameLengthOneTypes()
+        {
+            IList<Item> list = GenerateTestList();
             IList<Tuple<string>> tupleList = list.ToTupleList(x => x.Code);
             Assert.IsNotNull(tupleList);
             Assert.IsTrue(tupleList.Count == list.Count);
@@ -55,32 +59,7 @@ namespace GenericCore.Test
         [TestMethod]
         public void TestToTupleGeneratesNotNullListAndWithSameLengthTwoTypes()
         {
-            IList<Item> list =
-                new List<Item>
-                {
-                    new Item
-                    {
-                        Id = 1,
-                        Code = "A",
-                        Descr = "A letter",
-                        IsActive = true
-                    },
-                    new Item
-                    {
-                        Id = 2,
-                        Code = "B",
-                        Descr = "B letter",
-                        IsActive = true
-                    },
-                    new Item
-                    {
-                        Id = 3,
-                        Code = "C",
-                        Descr = "C letter",
-                        IsActive = true
-                    }
-                };
-
+            IList<Item> list = GenerateTestList();
             IList<Tuple<long, string>> tupleList = list.ToTupleList(x => x.Id, x => x.Code);
             Assert.IsNotNull(tupleList);
             Assert.IsTrue(tupleList.Count == list.Count);
@@ -89,32 +68,7 @@ namespace GenericCore.Test
         [TestMethod]
         public void TestToTupleGeneratesNotNullListAndWithSameLengthThreeTypes()
         {
-            IList<Item> list =
-                new List<Item>
-                {
-                    new Item
-                    {
-                        Id = 1,
-                        Code = "A",
-                        Descr = "A letter",
-                        IsActive = true
-                    },
-                    new Item
-                    {
-                        Id = 2,
-                        Code = "B",
-                        Descr = "B letter",
-                        IsActive = true
-                    },
-                    new Item
-                    {
-                        Id = 3,
-                        Code = "C",
-                        Descr = "C letter",
-                        IsActive = true
-                    }
-                };
-
+            IList<Item> list = GenerateTestList();
             IList<Tuple<long, string, bool>> tupleList = list.ToTupleList(x => x.Id, x => x.Code, x => x.IsActive);
             Assert.IsNotNull(tupleList);
             Assert.IsTrue(tupleList.Count == list.Count);
