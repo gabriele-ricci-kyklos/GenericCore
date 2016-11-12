be704ce5715c5687ad69c6f892c848dbc25b947a
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2016-11-12 14:49:55 +0100

    added ReadOnlyDictionary collection

---
+++ GenericCore/Exceptions/ReadOnlyException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GenericCore.Exceptions
+{
+    public class ReadOnlyException : Exception
+    {
+        public ReadOnlyException()
+            : base()
+        {
+        }
+
+        public ReadOnlyException(string message)
+            : base(message)
+        {
+        }
+
+        public ReadOnlyException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected ReadOnlyException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
