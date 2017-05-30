4f7d91a565b9146050178acfe734ff8719983eaf
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-05-30 14:53:02 +0200

    PatternMatcher class added

--- GenericCore.Test/Support/SupportTests.cs
+++ GenericCore.Test/Support/SupportTests.cs
@@ -21,5 +21,53 @@ namespace GenericCore.Test.Support.Strings
 
             Assert.AreEqual(3, Counter.Last);
         }
+
+        [TestMethod]
+        public void PatternMatcherTest()
+        {
+            PatternMatcher<int> getRentPrice = 
+                new PatternMatcher<int>()
+                    .Case<MotorCycle>(bike => 100 + bike.Cylinders * 10)
+                    .Case<Bicycle>(30)
+                    .Case<Car>(car => car.EngineType == EngineType.Diesel, car => 220 + car.Doors * 20)
+                    .Case<Car>(car => car.EngineType == EngineType.Gasoline, car => 200 + car.Doors * 20)
+                    .Default(0);
+
+            object[] vehicles = new object[] {
+                new Car { EngineType = EngineType.Diesel, Doors = 4 },
+                new Car { EngineType = EngineType.Gasoline, Doors = 3 },
+                new Bicycle(),
+                new MotorCycle { Cylinders = 2 },
+                new MotorCycle { Cylinders = 3 },
+            };
+
+            Assert.AreEqual(300, getRentPrice.Match(vehicles[0]));
+            Assert.AreEqual(260, getRentPrice.Match(vehicles[1]));
+            Assert.AreEqual(30, getRentPrice.Match(vehicles[2]));
+            Assert.AreEqual(120, getRentPrice.Match(vehicles[3]));
+            Assert.AreEqual(130, getRentPrice.Match(vehicles[4]));
+        }
+    }
+
+    public enum EngineType
+    {
+        Diesel,
+        Gasoline
+    }
+
+    public class Bicycle
+    {
+        public int Cylinders;
+    }
+
+    public class Car
+    {
+        public EngineType EngineType;
+        public int Doors;
+    }
+
+    public class MotorCycle
+    {
+        public int Cylinders;
     }
 }
---
+++ GenericCore/Support/PatternMatcher.cs
@@ -0,0 +1,80 @@
+﻿using GenericCore.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Support
+{
+    public class PatternMatcher<TOutput>
+    {
+        private IList<Tuple<Predicate<object>, Func<object, TOutput>>> _cases;
+
+        public PatternMatcher()
+        {
+            _cases = new List<Tuple<Predicate<object>, Func<object, TOutput>>>();
+        }
+
+        public PatternMatcher<TOutput> Case(Predicate<object> condition, Func<object, TOutput> function)
+        {
+            condition.AssertNotNull("condition");
+            function.AssertNotNull("function");
+
+            _cases.Add(new Tuple<Predicate<object>, Func<object, TOutput>>(condition, function));
+            return this;
+        }
+
+        public PatternMatcher<TOutput> Case<T>(Predicate<T> condition, Func<T, TOutput> function)
+        {
+            return 
+                Case
+                (
+                    o => o is T && condition((T)o),
+                    o => function((T)o)
+                );
+        }
+
+        public PatternMatcher<TOutput> Case<T>(Func<T, TOutput> function)
+        {
+            return 
+                Case
+                (
+                    o => o is T,
+                    o => function((T)o)
+                );
+        }
+
+        public PatternMatcher<TOutput> Case<T>(Predicate<T> condition, TOutput o)
+        {
+            return Case(condition, x => o);
+        }
+
+        public PatternMatcher<TOutput> Case<T>(TOutput o)
+        {
+            return Case<T>(x => o);
+        }
+
+        public PatternMatcher<TOutput> Default(Func<object, TOutput> function)
+        {
+            return Case(o => true, function);
+        }
+
+        public PatternMatcher<TOutput> Default(TOutput o)
+        {
+            return Default(x => o);
+        }
+
+        public TOutput Match(object o)
+        {
+            foreach (var tuple in _cases)
+            {
+                if (tuple.Item1(o))
+                {
+                    return tuple.Item2(o);
+                }
+            }
+                
+            throw new Exception("Failed to match");
+        }
+    }
+}
