3c086bd35afba42dc244d63640bfbafcb725ed6a
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-10-05 17:48:12 +0200

    added 'IsNullOrEmpty' extension method for data tables

---
+++ GenericCore/Support/ExtensionMethods/DataExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Support.ExtensionMethods
+{
+    public static class DataExtensionMethods
+    {
+        public static bool IsNullOrEmpty(this DataTable table)
+        {
+            return table.IsNull() || table.Rows.Count == 0;
+        }
+    }
+}
