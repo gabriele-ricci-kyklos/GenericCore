f5282478a20c3ead2839772e0245c4ff05aec19d
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-10-22 20:25:54 +0200

    added connection strings for excel reader and unit tests

---
+++ GenericCore.Test/Support/Excel/ExcelTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenericCore.Support.Excel;
+using System.Data;
+
+namespace GenericCore.Test.Support.Excel
+{
+    [TestClass]
+    public class ExcelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Temp\lol.xlsx";
+            ExcelReader reader = new ExcelReader(ExcelConnectionString.ToExcelV12(path));
+            string[] sheets = reader.ExtractSheetNames();
+            DataSet ds = reader.ReadData(sheets);
+        }
+    }
+}
---
+++ GenericCore/Support/Excel/ExcelConnectionString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Support.Excel
+{
+    public class ExcelConnectionString
+    {
+        public static string ToExcelV8(string excelFileName)
+        {
+            excelFileName.AssertNotNull("excelFileName");
+            return $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={excelFileName};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\"";
+        }
+
+        public static string ToExcelV12(string excelFileName, bool useHeaders = true)
+        {
+            excelFileName.AssertNotNull("excelFileName");
+            return $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={excelFileName};Extended Properties=\"Excel 12.0;HDR={(useHeaders ? "Yes" : "No")};IMEX=1;TypeGuessRows=0;ImportMixedTypes=Text;READONLY=TRUE;\"";
+        }
+
+        public static string ToExcelV14(string excelFileName)
+        {
+            excelFileName.AssertNotNull("excelFileName");
+            return $"Provider=Microsoft.ACE.OLEDB.14.0;Data Source={excelFileName};Extended Properties=\"Excel 12.0;HDR=YES\"";
+        }
+    }
+}
