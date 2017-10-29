66f5c012e3b0381ba0b150e00971a50783a303f4
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-10-29 12:34:19 +0100

    added WebPageDataRetriever class

---
+++ GenericCore.Test/Support/Web/WebTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenericCore.Support.Web;
+using System.IO;
+
+namespace GenericCore.Test.Support.Web
+{
+    [TestClass]
+    public class WebTests
+    {
+        [TestMethod]
+        public void TestPage()
+        {
+            string html = WebPageDataRetriever.RetrievePage("https://google.it/");
+            Assert.IsNotNull(html);
+        }
+
+        [TestMethod]
+        public void TestFile()
+        {
+            string localPath = @"C:\temp\master.zip";
+            bool succeded = WebPageDataRetriever.DownloadFile("https://github.com/gabriele-ricci-kyklos/FantasyFootballStatistics/archive/master.zip", localPath);
+            Assert.IsTrue(succeded);
+            Assert.IsTrue(File.Exists(localPath));
+        }
+    }
+}
