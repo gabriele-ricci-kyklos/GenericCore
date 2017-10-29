600afd0d9b745168c369ae7d8265a2a94eb15f01
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-10-29 12:34:31 +0100

    started ExternalExcelReader class

---
+++ GenericCore/Support/Excel/ExternalExcelReader.cs
@@ -0,0 +1,35 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Support.Excel
+{
+    public class ExternalExcelReader
+    {
+        public string FilePath { get; set; }
+        public ExternalExcelReader(string filePath)
+        {
+            filePath.AssertNotNull("filePath");
+            FilePath = filePath;
+        }
+
+        private DataSet ReadData()
+        {
+            DataSet resultSet = null;
+
+            using (var stream = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    resultSet = reader.AsDataSet();
+                }
+            }
+
+            return resultSet;
+        }
+    }
+}
---
+++ GenericCore/Support/Web/WebPageDataRetriever.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace GenericCore.Support.Web
+{
+    public static class WebPageDataRetriever
+    {
+        public static string RetrievePage(string pageUrl)
+        {
+            pageUrl.AssertNotNull("pageUrl");
+
+            string htmlCode = null;
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                htmlCode = client.DownloadString(pageUrl);
+            }
+
+            return htmlCode;
+        }
+
+        public static bool DownloadFile(string fileUrl, string localFilePath)
+        {
+            fileUrl.AssertNotNull("fileUrl");
+            localFilePath.AssertNotNull("localFilePath");
+
+            bool succeded = true;
+
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadFile(fileUrl, localFilePath);
+                }
+                catch(Exception)
+                {
+                    succeded = false;
+                }
+            }
+
+            return succeded;
+        }
+    }
+}
