e2389bef76c916ddce112c58271021ad96e2d485
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-11-05 12:35:25 +0100

    removed external excel data reader

--- GenericCore.Test/Support/Excel/ExcelTests.cs
+++ GenericCore.Test/Support/Excel/ExcelTests.cs
@@ -11,10 +11,10 @@ namespace GenericCore.Test.Support.Excel
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Temp\lol.xlsx";
+            string path = @"C:\Temp\data_sheet_20171029135142.xlsx";
             OleDbExcelReader reader = new OleDbExcelReader(ExcelOleDbConnectionString.ToExcelV12(path));
             string[] sheets = reader.ExtractSheetNames();
-            DataSet ds = reader.ReadData(sheets);
+            DataSet ds = reader.ReadMixedTypesData(sheets);
         }
     }
 }
