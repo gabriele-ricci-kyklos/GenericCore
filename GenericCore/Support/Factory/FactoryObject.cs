23c9e68450f5d488d3c5680ba64ae1ff353198c6
Gabriele Ricci <gabriele.ricci@kyklos-group.com>
2017-12-04 14:26:30 +0100

    added ObjectsFactory

---
+++ GenericCore/Support/Factory/FactoryObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCore.Support.Factory
+{
+    public class FactoryObject
+    {
+        public string Name { get; set; }
+        public string Assembly { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ GenericCore/Support/Factory/ObjectsFactoryException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GenericCore.Support.Factory
+{
+    public class ObjectsFactoryException : Exception
+    {
+        public ObjectsFactoryException()
+            : base()
+        {
+        }
+
+        public ObjectsFactoryException(string message)
+            : base(message)
+        {
+        }
+
+        public ObjectsFactoryException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected ObjectsFactoryException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
