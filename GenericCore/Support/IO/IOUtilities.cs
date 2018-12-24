e44ec2aba786fc66d203d9a76a561f69dd9ed208
gabriele-ricci-kyklos <gabriele.ricci@kyklos-group.com>
2018-12-24 20:25:15 +0100

    added IOUtilities class

---
+++ GenericCore/Support/IO/IOUtilities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCore.Support
+{
+    //credits: https://stackoverflow.com/a/50405099/4499267
+
+    //TODO: do some tests
+    public class IOUtilities
+    {
+        public static bool EmptyFolder(string pathName)
+        {
+            bool errors = false;
+            DirectoryInfo dir = new DirectoryInfo(pathName);
+
+            foreach (FileInfo fi in dir.EnumerateFiles())
+            {
+                try
+                {
+                    fi.IsReadOnly = false;
+                    fi.Delete();
+
+                    //Wait for the item to disapear (avoid 'dir not empty' error).
+                    while (fi.Exists)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        fi.Refresh();
+                    }
+                }
+                catch (IOException)
+                {
+                    errors = true;
+                }
+            }
+
+            foreach (DirectoryInfo di in dir.EnumerateDirectories())
+            {
+                try
+                {
+                    EmptyFolder(di.FullName);
+                    di.Delete();
+
+                    //Wait for the item to disapear (avoid 'dir not empty' error).
+                    while (di.Exists)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        di.Refresh();
+                    }
+                }
+                catch (IOException)
+                {
+                    errors = true;
+                }
+            }
+
+            return !errors;
+        }
+
+        public static bool DeleteFolder(string pathName)
+        {
+            if(!EmptyFolder(pathName))
+            {
+                return false;
+            }
+
+            Directory.Delete(pathName, true);
+            return true;
+        }
+    }
+}
