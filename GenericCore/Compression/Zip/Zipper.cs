c51aaf701e6dfc3f92db1cdfa747f902285dc85d
gabriele-ricci-kyklos <gabriele.ricci@kyklos-group.com>
2018-12-24 20:25:02 +0100

    updated Zipper

--- GenericCore/Compression/Zip/Zipper.cs
+++ GenericCore/Compression/Zip/Zipper.cs
@@ -11,35 +11,25 @@ namespace GenericCore.Compression.Zip
 {
     public static class Zipper
     {
-        public static byte[] ZipStringToBytes(string stringToZip)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(stringToZip);
-            return ZipBytes(buffer);
-        }
+        #region Zipper
 
         public static string ZipString(string stringToZip)
         {
-            byte[] buffer = Encoding.UTF8.GetBytes(stringToZip);
-            return ZipBytesToString(buffer);
-        }
-
-        public static string ZipBytesToString(byte[] bytesToZip)
-        {
-            byte[] zippedData = ZipBytes(bytesToZip);
-            return Convert.ToBase64String(zippedData);
-        }
-
-        public static byte[] ZipBytes(byte[] bytesToZip)
-        {
-            if (bytesToZip.IsNullOrEmptyList())
+            if (stringToZip == null)
             {
                 return null;
             }
 
+            if (stringToZip.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes(stringToZip);
             var memoryStream = new MemoryStream();
             using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
             {
-                gZipStream.Write(bytesToZip, 0, bytesToZip.Length);
+                gZipStream.Write(buffer, 0, buffer.Length);
             }
 
             memoryStream.Position = 0;
@@ -49,40 +39,27 @@ namespace GenericCore.Compression.Zip
 
             var gZipBuffer = new byte[compressedData.Length + 4];
             Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length);
-            Buffer.BlockCopy(BitConverter.GetBytes(bytesToZip.Length), 0, gZipBuffer, 0, 4);
-
-            return gZipBuffer;
+            Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gZipBuffer, 0, 4);
+            return Convert.ToBase64String(gZipBuffer);
         }
 
         public static string UnZipString(string zippedString)
         {
-            byte[] unzippedBytes = UnZipStringToBytes(zippedString);
-            return Encoding.UTF8.GetString(unzippedBytes);
-        }
-
-        public static string UnZipBytesToString(byte[] zippedBytes)
-        {
-            byte[] unzippedBytes = UnZipBytes(zippedBytes);
-            return Encoding.UTF8.GetString(unzippedBytes);
-        }
-
-        public static byte[] UnZipStringToBytes(string zippedString)
-        {
-            byte[] buffer = Convert.FromBase64String(zippedString);
-            return UnZipBytes(buffer);
-        }
-
-        public static byte[] UnZipBytes(byte[] zippedBytes)
-        {
-            if (zippedBytes.IsNullOrEmptyList())
+            if (zippedString == null)
             {
                 return null;
             }
 
+            if (zippedString.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+
+            byte[] gZipBuffer = Convert.FromBase64String(zippedString);
             using (var memoryStream = new MemoryStream())
             {
-                int dataLength = BitConverter.ToInt32(zippedBytes, 0);
-                memoryStream.Write(zippedBytes, 4, zippedBytes.Length - 4);
+                int dataLength = BitConverter.ToInt32(gZipBuffer, 0);
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
 
                 var buffer = new byte[dataLength];
 
@@ -92,35 +69,77 @@ namespace GenericCore.Compression.Zip
                     gZipStream.Read(buffer, 0, buffer.Length);
                 }
 
-                return buffer;
+                return Encoding.UTF8.GetString(buffer);
             }
         }
 
-        //public static void CreateZipFileFromFileList(string zipFileName, IList<string> fileNames)
-        //{
-        //    CreateZipFileFromFileList(zipFileName, fileNames, null, false);
-        //}
+        public static void UnZipSingleFileContentToStream(byte[] fileContent, Stream outstream)
+        {
+            fileContent.AssertNotNull(nameof(fileContent));
 
-        //public static void CreateZipFileFromFileList(string zipFileName, IList<string> fileNames, IList<string> descriptions, bool flatPath)
-        //{
-        //    List<FileInfo> files = new List<FileInfo>();
-        //    foreach (var file in fileNames.Distinct())
-        //    {
-        //        if (!File.Exists(file))
-        //        {
-        //            continue;
-        //        }
+            using (MemoryStream ms = new MemoryStream(fileContent))
+            using (ZipFile zipFile = ZipFile.Read(ms))
+            {
+                (zipFile.Count == 1).AssertIsTrue("Zip contains more than one file");
+                ZipEntry zipEntry = zipFile.First();
+                zipEntry.IsDirectory.AssertIsFalse("Zip content is a folder!");
+                zipEntry.Extract(outstream);
+            }
+        }
 
-        //        FileInfo fi = new FileInfo(file);
+        public static Stream ZipSingleFileContent(byte[] fileContent, string entryName)
+        {
+            fileContent.AssertNotNull(nameof(fileContent));
 
-        //        if ((fi.Attributes & FileAttributes.Directory) == 0)
-        //        {
-        //            files.Add(fi);
-        //        }
-        //    }
+            var ms = new MemoryStream();
+            using (var zipStream = new ZipFile())
+            {
+                zipStream.AddEntry(entryName, fileContent);
+                zipStream.Save(ms);
+            }
+            ms.Position = 0;
+            return ms;
+        }
 
-        //    CreateZipFileFromFileListImpl(zipFileName, files, descriptions, true, flatPath);
-        //}
+        public static byte[] ZipByteArray(byte[] fileContent, string entryName)
+        {
+            using (MemoryStream ms = ZipSingleFileContent(fileContent, entryName) as MemoryStream)
+            {
+                return ms.ToArray();
+            }
+        }
+
+        public static void CreateZipFileFromFileList(string zipFileName, IList<string> fileNames)
+        {
+            CreateZipFileFromFileList(zipFileName, fileNames, null, false);
+        }
+
+        public static void CreateZipFileFromFileListWithFileNameOnly(string zipFileName, IList<string> fileNames)
+        {
+            CreateZipFileFromFileList(zipFileName, fileNames, null, true, true);
+        }
+
+        public static void CreateZipFileFromFileList(string zipFileName, IList<string> fileNames, IList<string> descriptions, bool flatPath, bool fileNameOnly = false)
+        {
+            List<FileInfo> files = new List<FileInfo>();
+            foreach (var file in fileNames.Distinct())
+            {
+                if (!File.Exists(file))
+                {
+                    continue;
+                }
+
+                FileInfo fi = new FileInfo(file);
+
+                if ((fi.Attributes & FileAttributes.Directory) == 0)
+                {
+                    files.Add(fi);
+                }
+            }
+
+            //CreateZipFileFromFileListImpl(zipFileName, files, descriptions, true, flatPath);
+            CreateZipFileFromFileListImpl(zipFileName, files, descriptions, true, flatPath, fileNameOnly);
+        }
 
         public static byte[] CreateZipFromFileContentList(IList<Tuple<string, byte[]>> fileContentList)
         {
@@ -141,7 +160,7 @@ namespace GenericCore.Compression.Zip
                     }
                     MemoryStream ms = new MemoryStream();
                     zip.Save(ms);
-                    return ms.GetBuffer();
+                    return ms.ToArray();
                 }
             }
             catch (Exception)
@@ -150,37 +169,37 @@ namespace GenericCore.Compression.Zip
             }
         }
 
-        //public static byte[] CreateZipContentFromFolder(string directory, string entryDirectory, string pattern, bool recursive)
-        //{
-        //    try
-        //    {
-        //        using (ZipFile zip = new ZipFile())
-        //        {
-        //            zip.CompressionLevel = CompressionLevel.Default;
+        public static byte[] CreateZipContentFromFolder(string directory, string entryDirectory, string pattern, bool recursive)
+        {
+            try
+            {
+                using (ZipFile zip = new ZipFile())
+                {
+                    zip.CompressionLevel = CompressionLevel.Default;
 
-        //            IList<string> fileList = Utilities.GetFilesInFolder(directory, pattern, recursive);
+                    IList<string> fileList = GetFilesInFolder(directory, pattern, recursive);
 
-        //            IList<string> fileListWithNewPath = ModifyFilesPath(fileList, entryDirectory);
+                    IList<string> fileListWithNewPath = ModifyFilesPath(fileList, entryDirectory);
 
-        //            int index = 0;
+                    int index = 0;
 
-        //            foreach (var item in fileList)
-        //            {
-        //                byte[] content = File.ReadAllBytes(item);
-        //                ZipEntry entry = null;
-        //                entry = zip.AddEntry(fileListWithNewPath[index], content);
-        //                index++;
-        //            }
-        //            MemoryStream ms = new MemoryStream();
-        //            zip.Save(ms);
-        //            return ms.GetBuffer();
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
+                    foreach (var item in fileList)
+                    {
+                        byte[] content = File.ReadAllBytes(item);
+                        ZipEntry entry = null;
+                        entry = zip.AddEntry(fileListWithNewPath[index], content);
+                        index++;
+                    }
+                    MemoryStream ms = new MemoryStream();
+                    zip.Save(ms);
+                    return ms.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
         private static IList<string> ModifyFilesPath(IList<string> fileList, string entryDirectory)
         {
@@ -194,96 +213,153 @@ namespace GenericCore.Compression.Zip
         }
 
         //private static void CreateZipFileFromFileListImpl(string zipFileName, IList<FileInfo> fileNames, IList<string> comments, bool overwriteIfExists, bool flatPath)
-        //{
-        //    const string zipExt = ".zip";
-        //    try
-        //    {
-        //        //if file name extension terminates with something different from '.zip' 
-        //        //this will change it because it is needed from ionic.zip as save file param.
-        //        if (Path.HasExtension(zipFileName))
-        //        {
-        //            string ext = Path.GetExtension(zipFileName);
-        //            zipFileName = (ext.Equals(zipExt)) ? zipFileName : Path.ChangeExtension(zipFileName, zipExt);
-        //        }
-        //        else
-        //        {
-        //            zipFileName += zipExt;
-        //        }
+        private static void CreateZipFileFromFileListImpl(string zipFileName, IList<FileInfo> fileNames, IList<string> comments, bool overwriteIfExists, bool flatPath, bool onlyFileName)
+        {
+            const string zipExt = ".zip";
+            try
+            {
+                //if file name extension terminates with something different from '.zip' 
+                //this will change it because it is needed from ionic.zip as save file param.
+                if (Path.HasExtension(zipFileName))
+                {
+                    string ext = Path.GetExtension(zipFileName);
+                    zipFileName = (ext.Equals(zipExt)) ? zipFileName : Path.ChangeExtension(zipFileName, zipExt);
+                }
+                else
+                {
+                    zipFileName += zipExt;
+                }
 
-        //        using (ZipFile zip = new ZipFile())
-        //        {
-        //            zip.CompressionLevel = CompressionLevel.Default;
+                Dictionary<string, int> fileSet = new Dictionary<string, int>();
 
-        //            int i = 0;
-        //            foreach (FileInfo filename in fileNames)
-        //            {
-        //                ZipEntry entry = null;
-        //                if (flatPath)
-        //                {
-        //                    var content = File.ReadAllBytes(filename.FullName);
-        //                    entry = zip.AddEntry(Utilities.FlatFileName(filename), content);
-        //                }
-        //                else
-        //                {
-        //                    entry = zip.AddFile(filename.FullName);
-        //                }
-        //                entry.Comment = comments.Return(x => x.ElementAtOrDefault(i).ToEmptyIfNull(), string.Empty);
-        //                ++i;
-        //            }
+                using (ZipFile zip = new ZipFile())
+                {
+                    zip.CompressionLevel = CompressionLevel.Default;
 
-        //            if (File.Exists(zipFileName))
-        //            {
-        //                if (overwriteIfExists)
-        //                {
-        //                    File.Delete(zipFileName);
-        //                }
-        //                else
-        //                {
-        //                    throw new Exception("File {0} already exists.".FormatWith(zipFileName));
-        //                }
-        //            }
-        //            zip.Save(zipFileName);
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //}
+                    int i = 0;
+                    foreach (FileInfo filename in fileNames)
+                    {
+                        ZipEntry entry = null;
+                        if (flatPath)
+                        {
+                            var content = File.ReadAllBytes(filename.FullName);
+                            //entry = zip.AddEntry(Utilities.FlatFileName(filename), content);
 
-        public static IList<byte[]> GetFileContentFromZipFile(string filePath, string fileName = null)
+                            string fileEntryName = string.Empty;
+                            if (onlyFileName)
+                            {
+                                int counter;
+                                if (fileSet.TryGetValue(filename.Name, out counter))
+                                {
+                                    ++counter;
+                                    fileEntryName = $"{Path.GetFileNameWithoutExtension(filename.Name)}_{counter}{filename.Extension}";
+                                    fileSet[filename.Name] = counter;
+                                }
+                                else
+                                {
+                                    fileEntryName = filename.Name;
+                                    fileSet.Add(filename.Name, 0);
+                                }
+                            }
+                            else
+                            {
+                                fileEntryName = FlatFileName(filename);
+                            }
+
+                            entry = zip.AddEntry(fileEntryName, content);
+                        }
+                        else
+                        {
+                            entry = zip.AddFile(filename.FullName);
+                        }
+                        entry.Comment = comments.Return(x => x.ElementAtOrDefault(i).ToEmptyIfNull(), string.Empty);
+                        ++i;
+                    }
+
+                    if (File.Exists(zipFileName))
+                    {
+                        if (overwriteIfExists)
+                        {
+                            File.Delete(zipFileName);
+                        }
+                        else
+                        {
+                            throw new ArgumentException($"File {zipFileName} already exists");
+                        }
+                    }
+                    zip.Save(zipFileName);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region UnZipper
+
+        public static IList<Tuple<string, byte[]>> GetFileContentsFromZipFile(string filePath, string fileName = null)
         {
             if (!File.Exists(filePath))
             {
                 throw new FileNotFoundException("The file hasn't been found", filePath);
             }
 
-            return GetFileContentFromZipFile(File.ReadAllBytes(filePath), fileName);
+            byte[] fileContent = File.ReadAllBytes(filePath);
+
+            return GetFileContentsFromZipFile(fileContent, fileName);
         }
 
-        public static IList<byte[]> GetFileContentFromZipFile(byte[] fileContent, string fileName = null)
+        public static IList<Tuple<string, byte[]>> GetFileContentsFromZipFile(byte[] fileContent, string fileName = null)
         {
             fileContent.AssertNotNull("fileContent");
 
-            IList<byte[]> files = new List<byte[]>();
+            IList<Tuple<string, byte[]>> files = new List<Tuple<string, byte[]>>();
 
             using (MemoryStream ms = new MemoryStream(fileContent))
             using (ZipFile zip = ZipFile.Read(ms))
             {
                 foreach (ZipEntry e in zip)
                 {
-                    if (fileName == null || e.FileName == fileName)
+                    if (!e.IsDirectory && (fileName == null || e.FileName == fileName))
                     {
                         using (MemoryStream m = new MemoryStream())
                         {
                             e.Extract(m);
-                            files.Add(m.ToArray());
+                            files.Add(Tuple.Create(e.FileName, m.ToArray()));
                         }
                     }
                 }
             }
 
             return files;
+        }
+
+        #endregion
+
+        private static IList<string> GetFilesInFolder(string file, string searchPattern, bool recursive)
+        {
+            if (!File.Exists(file) && !Directory.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            if (Directory.Exists(file))
+            {
+                return
+                    Directory
+                    .GetFiles(file, searchPattern, recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                    .ToList();
+            }
+
+            return file.AsList();
+        }
+
+        private static string FlatFileName(FileInfo fi)
+        {
+            return fi.FullName.Replace(Path.DirectorySeparatorChar, '_').Replace(":", string.Empty);
         }
     }
 }
