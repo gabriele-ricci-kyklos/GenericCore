d0ca35e4c6dd7a94288a89cf2477c9f20971e9be
gabriele-ricci-kyklos <gabriele.ricci@kyklos-group.com>
2019-01-31 18:35:39 +0100

    added Linq and Property equality comparers

---
+++ GenericCore/Support/EqualityComparers/LinqEqualityComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCore.Support.EqualityComparers
+{
+    public class LinqEqualityComparer<T> : EqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _eqComparer;
+        private readonly Func<T, int> _hashFunction;
+
+        public LinqEqualityComparer(Func<T, T, bool> eqComparer)
+            : this(eqComparer, x => x.GetHashCode())
+        {
+        }
+
+        public LinqEqualityComparer(Func<T, T, bool> eqComparer, Func<T, int> hashFunction)
+        {
+            _eqComparer = eqComparer;
+            _hashFunction = hashFunction;
+        }
+
+        public override bool Equals(T x, T y)
+        {
+            return _eqComparer(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return _hashFunction(obj);
+        }
+    }
+}
---
+++ GenericCore/Support/EqualityComparers/PropertyEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCore.Support.EqualityComparers
+{
+    public class PropertyEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> _eqComparer;
+        private readonly Func<T, int> _hashFunction;
+
+        public PropertyEqualityComparer(Func<T, object> propertyFx)
+        {
+            _eqComparer = (x, y) => propertyFx(x).Equals(propertyFx(y));
+            _hashFunction = (x) => propertyFx(x).GetHashCode();
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _eqComparer(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return _hashFunction(obj);
+        }
+    }
+}
